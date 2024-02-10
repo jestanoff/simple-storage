ab84487b2862f3c660e8fb1da986a3ddf6f988a8
Stefan Dzhestanov <stefan.dzhestanov@dojo.tech>
2024-02-10 15:11:46 +0000

    First commit

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage{value: 10}();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
